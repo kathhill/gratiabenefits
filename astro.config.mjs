4763737e0b68e49892a4f00839479b24e5276a99
kathhill <kath.hill@sbcglobal.net>
2024-10-29 12:45:01 -0700

    feat: Add icon library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        icon(),
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler"
+                }
+            }
+        }
+    }
+});
